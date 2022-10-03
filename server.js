42b369a049826252040e44bb7085aa0be9ca21f9
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-10-03 05:53:37 +0100

    serve static asset

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ var myApp = require("./myApp");
 var express = require("express");
 var app = express();
 
-app.get("/", function (req, res) {
+/*app.get("/", function (req, res) {
   res.send("Hello Express");
 });
+*/
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.use("/public", express.static(__dirname + "/public"));
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function (req, res, next) {
